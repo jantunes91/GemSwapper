76a2cedab53fa59c27cc0eb61203c5f92c596e87
João Eiras Antunes <jantunes91@gmail.com>
2016-06-29 14:10:33 +0100

    Added a function that drops pieces when a sequence is destroyed. still needs work

--- main.cpp
+++ main.cpp
@@ -97,6 +97,12 @@ public:
 
 	bool isRemoved();
 
+	void setToUpdate(bool toUpdate);
+
+	bool toUpdate();
+
+	int updateY;
+
 	//Handles mouse event
 	void handleEvent(SDL_Event* e);
 
@@ -115,6 +121,8 @@ private:
 	int type;
 
 	bool removed;
+
+	bool updatePosition;
 };
 
 //Starts up SDL and creates window
@@ -131,6 +139,8 @@ bool checkSequence();
 
 //Removes a previously found sequence, given it's end and it's lenght
 void removeSequence(int x, int y, int lenght, int orientation);
+
+void dropDownSquares();
 
 //Frees media and shuts down SDL
 void close();
@@ -279,6 +289,9 @@ LButton::LButton()
 	type = rand() % 5;
 
 	removed = false;
+
+	updatePosition = false;
+	updateY = 0;
 }
 
 void LButton::setPosition(int x, int y)
@@ -315,6 +328,16 @@ void LButton::setRemoved(bool isRemoved)
 bool LButton::isRemoved()
 {
 	return removed;
+}
+
+void LButton::setToUpdate(bool toUpdate)
+{
+	updatePosition = toUpdate;
+}
+
+bool LButton::toUpdate()
+{
+	return updatePosition;
 }
 
 void LButton::handleEvent(SDL_Event* e)
@@ -624,6 +647,15 @@ void swapSquares()
 			gPressedButtons[0]->setType(gPressedButtons[1]->getType());
 			gPressedButtons[1]->setType(tempType);
 		}
+		else
+		{
+			bool test = true;
+			//while (test)
+			//{
+				dropDownSquares();
+				//checkSequence();
+			//}
+		}
 		gPressedButtons[0]->setPressed(false);
 		gPressedButtons[1]->setPressed(false);
 		pressedCount = 0;
@@ -637,7 +669,7 @@ bool checkSequence()
 	// Checks for duplicates in rows
 	int count = 0;
 	int location = -1;
-	int type;
+	int type = -2;
 	for (int y = 0; y < 8; y++)
 	{
 		for (int x = 0; x < 8; x++)
@@ -677,7 +709,7 @@ bool checkSequence()
 	// Checks for duplicates in columns
 	count = 0;
 	location = -1;
-	type;
+	type = -2;
 	for (int i = 0; i < 8; i++)
 	{
 		for (int j = 0; j < 8; j++)
@@ -698,7 +730,7 @@ bool checkSequence()
 				}
 				else
 				{
-					if (count > 2) {
+					if (count > 2 && gButtons[i + j * 8].getType() != -1) {
 						removeSequence(i, location, count, 1);
 						sequenceFound = true;
 					}
@@ -745,6 +777,78 @@ void removeSequence(int x, int y, int lenght, int orientation)
 
 }
 
+void dropDownSquares()
+{
+	for (int x = 0; x < 8; x++)
+	{
+		// We go from the bottom up
+		for (int y = 7; y >= 0; y--)
+		{
+			// If the current square is empty, every square above it should fall one position
+			if (gButtons[x + y * 8].isRemoved())
+			{
+				for (int k = y - 1; k >= 0; k--)
+				{
+					gButtons[x + k * 8].setToUpdate(true);
+					gButtons[x + k * 8].updateY++;
+				}
+			}
+		}
+
+		// Now that each square has its new position in their updateY property,
+		// let's move them to that final position
+
+		for (int y = 7; y >= 0; y--)
+		{
+			bool testupdate = gButtons[x + y * 8].toUpdate();
+			bool testremoved = gButtons[x + y * 8].isRemoved();
+			// If the square is not empty and has to fall, move it to the new position
+			if (gButtons[x + y * 8].toUpdate() == true && gButtons[x + y * 8].isRemoved() == false)
+			{
+				int y0 = gButtons[x + y * 8].updateY;
+
+				gButtons[x + (y+y0) * 8].setType(gButtons[x + y * 8].getType());
+				gButtons[x + y * 8].setType(-1);
+				gButtons[x + y * 8].setRemoved(true);
+				gButtons[x + (y + y0) * 8].setRemoved(false);
+				gButtons[x + y * 8].setToUpdate(false);
+				gButtons[x + (y + y0) * 8].setToUpdate(false);
+			}
+		}
+
+		// Finally, let's count how many new empty spaces there are so we can fill
+		// them with new random gems
+		int emptySpaces = 0;
+
+		/*// We start counting from top to bottom. Once we find a square, we stop counting
+		for (int y = 0; y < 8; ++y)
+		{
+			if (!board[x][y].destroy)
+				break;
+
+			emptySpaces++;
+		}
+		
+		// Again from top to bottom, fill the emtpy squares, assigning them a
+		// proper position outta screen for the animation to work
+		for (int y = 0; y < 8; y++)
+		{
+			if (gButtons[x + y * 8].isRemoved())
+			{
+				gButtons[x + y * 8].setType(rand() % 5);
+				gButtons[x + y * 8].setRemoved(false);
+				gButtons[x + y * 8].updatePosition = false;
+				gButtons[x + y * 8].updateY = y;
+				/*board[x][y].x = x;
+				board[x][y].y = y;
+				board[x][y].updateY = y;
+				board[x][y].updatePosition = false;
+				board[x][y].destroy = false;
+			}
+		}*/
+	}
+}
+
 void close()
 {
 	//Free loaded images
@@ -787,7 +891,11 @@ int main(int argc, char* args[])
 			//Event handler
 			SDL_Event e;
 
-			checkSequence();
+			while (checkSequence())
+			{
+				dropDownSquares();
+				//generate new gems
+			}
 
 			//While application is running
 			while (!quit)
